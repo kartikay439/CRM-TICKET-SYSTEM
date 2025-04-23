e23ee52627dc7961af020518d38af73cbc7f4372
ANUBHAV-03042004 <114772861+ANUBHAV-03042004@users.noreply.github.com>
2025-04-23 16:06:56 +0530

    App.jsx update

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,30 +1,41 @@
+import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import { Register } from './pages/register/register.jsx';
-import { Homepage } from './home/homepage.jsx';
-import { Login } from './pages/login/login.jsx';
-import {Forgot_Password} from './pages/forgot_password/forgot_password.jsx';
-import { Reset_Password } from './pages/reset_password/reset_password.jsx';
-import {User_dashboard} from './User-functionalities/User-dashboard.jsx';
-import {Add_new_ticket} from './User-functionalities/add_new_ticket/Add_new_ticket.jsx';
-import './App.css';
+import { Register } from './pages/register/register';
+import { Homepage } from './home/homepage';
+import { Login } from './pages/login/login';
+import {Forgot_Password} from './pages/forgot_password/forgot_password';
+import { Reset_Password } from './pages/reset_password/reset_password';
+import {User_dashboard} from './User-functionalities/User-dashboard';
+import {Add_new_ticket} from './User-functionalities/add_new_ticket/Add_new_ticket';
 import {Profile} from "./User-functionalities/profile/profile";
+import { Otp_Verify } from './pages/otp/otp_verify';
+import { Email } from './pages/otp/email';
 import {Feedback} from './User-functionalities/feedback/feedback';
-import {Verify} from "./pages/verify/verify.jsx";
-
+import {AdminRegister} from './Admin_Pages/AdminRegister';
+import {AdminLogin} from './Admin_Pages/AdminLogin';
+import {AdminDashboard} from './Admin-functionalities/Admin_dashboard';
+import {AdminProfile} from './Admin-functionalities/Admin_Profile';
+import {Add_new_client} from './Admin-functionalities/add_new_client/Add_new_client';
 function App() {
   return (
     <Router>
       <Routes>
         <Route path="/" element={<Homepage />} />
         <Route path="/register" element={<Register />} />
-        <Route path="/verify" element={<Verify />} />
         <Route path="/login" element={<Login />} />
+        <Route path="/otp-verify" element={<Otp_Verify />} />
         <Route path="/forgot" element={<Forgot_Password />} />
         <Route path="/reset" element={<Reset_Password />} />
         <Route path="/user-dashboard" element={<User_dashboard />} />
         <Route path="/add_new_ticket" element={<Add_new_ticket />} />
         <Route path="/profile" element={<Profile />} />
         <Route path="/feedback" element={<Feedback/>} />
+        <Route path="/email" element={<Email/>} />
+        <Route path="/register/admin" element={<AdminRegister/>} />
+        <Route path="/login/admin" element={<AdminLogin/>} />
+        <Route path="/admin-dashboard" element={<AdminDashboard/>} />
+        <Route path="/admin-profile" element={<AdminProfile/>} />
+        <Route path="/add_new_client" element={<Add_new_client/>} />
       </Routes>
     </Router>
   );
